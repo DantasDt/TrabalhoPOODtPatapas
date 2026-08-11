7b5e7e607cabc0c73df756b044adaaab396eb203
Dantas <dantas.69.g@gmail.com>
2026-08-11 19:48:48 -0300

    Update Contato.py

---
+++ TrabalhoDtPt/Contato.py
@@ -0,0 +1,8 @@
+class Contato:
+    def __init__(self, tipo, contato):
+        self.__tipo = tipo
+        self.__contato = contato
+
+    def MostraContato(self):
+        self.__contato
+        
