285ea1fb0984d1658f1b45e200ad58d6231e3dcf
Dantas <dantas.69.g@gmail.com>
2026-08-11 18:15:12 -0300

    Update Jogador.py

---
+++ TrabalhoDtPt/Jogador.py
@@ -0,0 +1,10 @@
+class Jogador:
+    def __init__(self, nome, idade, posicao, numero, contrato):
+        self.__nome = nome
+        self.__idade = idade
+        self.__posicao = posicao
+        self.__numero = numero
+        self.__contrato = contrato
+
+    def AlterarNumero(self):
+        self.__numero
