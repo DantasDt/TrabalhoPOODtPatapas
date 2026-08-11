b3c04e487e127de76288b899b2fc149497ccd2ec
Dantas <dantas.69.g@gmail.com>
2026-08-11 18:14:57 -0300

    Update Time.py

---
+++ TrabalhoDtPt/Time.py
@@ -0,0 +1,17 @@
+class Time:
+    def __init__(self, nome, anoF, titulos):
+        self.__nome = nome
+        self.__anoF = anoF
+        self.__titulos = titulos
+
+    def DisputarPartida(self):
+        self.__nome
+
+    def Escalacao(self):
+        self.__nome
+
+    def Contratar(self):
+        self.__nome
+
+    def JogadorDestaque(self):
+        self.__nome
