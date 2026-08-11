465f538db35798b62a895066cb70bec9d8db01fa
Dantas <dantas.69.g@gmail.com>
2026-08-11 19:48:40 -0300

    Update Tecnico.py

---
+++ TrabalhoDtPt/Tecnico.py
@@ -0,0 +1,15 @@
+class Tecnico:
+    def __init__(self, nome, titulos, AnosDeTrabalho, idade, time, esquema):
+        self.__nome = nome
+        self.__titulos = titulos
+        self.__AnosDeTrabalho = AnosDeTrabalho
+        self.__idade = idade
+        self.__time = time
+        self.__esquema = esquema
+
+    def DefinirEsquema(self):
+        self.__esquema
+
+    def DarInstrucao(self):
+        self.__nome
+        
