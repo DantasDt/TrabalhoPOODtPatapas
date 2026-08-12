c16189e0b6b96374c3472ab85b51661014182acc
Dantas <dantas.69.g@gmail.com>
2026-08-12 17:20:46 -0300

    Update Base.py

--- TrabalhoDtPt/Base.py
+++ TrabalhoDtPt/Base.py
@@ -1,12 +1,30 @@
 from Time import Time
+from Jogador import Jogador
+from random import *
 
 class Base(Time):
-    def __init__(self, nome, anoF, titulos, categoria):
-        super().__init__(nome, anoF, titulos)
+    def __init__(self, nome, anoF, titulos, tecnico, categoria):
+        super().__init__(nome, anoF, titulos, tecnico)
         self.__categoria = categoria
+        self.__jogadores = []
 
     def JogadorDestaque(self):
-        self.__categoria
+        return f"Jogador Destaque: {self.__jogadores[randint(len(self.__jogadores))]}"
 
     def Promover(self):
         self.__categoria
+
+    def GetCategoria(self):
+        return self.__categoria
+
+    def Contratar(self):
+        NomeJ = input("Informe o nome do jogador: ")
+        IdadeJ = input("Informe a idade do jogador: ")
+        PosicaoJ = input("Informe a posicao do jogador: ")
+        NumeroJ = input("Informe o numero do jogador: ")
+        ContratoJ = input("Informe o contrato do jogador: ")
+        jogador = Jogador(NomeJ, IdadeJ, PosicaoJ, NumeroJ, ContratoJ)
+        self.__jogadores.append(jogador)
+
+    def AdicionarJogador(self, jogador):
+        self.__jogadores.append(jogador)
