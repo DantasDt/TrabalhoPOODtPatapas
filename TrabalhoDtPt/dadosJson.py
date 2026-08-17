85e5febe15c4c8505f3941a45aebeadd54cc3672
Dantas <dantas.69.g@gmail.com>
2026-08-17 13:34:17 -0300

    Update dadosJson.py

--- TrabalhoDtPt/dadosJson.py
+++ TrabalhoDtPt/dadosJson.py
@@ -2,7 +2,8 @@ import json
 from Time import Time
 from Base import Base
 from Jogador import Jogador
-
+from Tecnico import Tecnico
+from Contato import Contato
 
 def SalvarTime(obj):
     dados = {
@@ -10,9 +11,19 @@ def SalvarTime(obj):
             "nome": obj.GetNome(),
             "anoF": obj.GetAnoF(),
             "titulos": obj.GetTitulos(),
-            "tecnico": obj.GetTecnico()
+            "tecnico": {
+                "nome": obj.GetTecnico().GetNome(),
+                "titulos": obj.GetTecnico().GetTitulos(),
+                "anosDeTrabalho": obj.GetTecnico().GetAnosDeTrabalho(),
+                "idade": obj.GetTecnico().GetIdade(),
+                "time": obj.GetTecnico().GetTime(),
+                "esquema": obj.GetTecnico().GetEsquema()
+            },
+            "contato" : {
+                "tipo": obj.GetContato().GetTipo(),
+                "contato": obj.GetContato().GetContato()
+            }
         },
-
         "jogadores": []
     }
 
@@ -29,14 +40,34 @@ def SalvarTime(obj):
         json.dump(dados, arquivo, indent=4, ensure_ascii=False)
 
 def CarregarTime():
-    with open("Time.json", "r", encoding="utf-8") as arquivo:
-        dados = json.load(arquivo)
+    try:
+        with open("Time.json", "r", encoding="utf-8") as arquivo:
+            dados = json.load(arquivo)
+    except (FileNotFoundError, json.JSONDecodeError):
+        tecnico = Tecnico("", "", 0, "", "", "")
+        return Time("", 0, "", "", "")
 
-        obj = Time(
-            dados["time"]["nome"],
-            dados["time"]["anoF"],
-            dados["time"]["titulos"],
-            dados["time"]["tecnico"])
+    contato = Contato(
+        dados["time"]["contato"]["tipo"],
+        dados["time"]["contato"]["contato"]
+    )
+
+    tecnico = Tecnico(
+        dados["time"]["tecnico"]["nome"],
+        dados["time"]["tecnico"]["titulos"],
+        dados["time"]["tecnico"]["anosDeTrabalho"],
+        dados["time"]["tecnico"]["idade"],
+        dados["time"]["tecnico"]["time"],
+        dados["time"]["tecnico"]["esquema"]
+    )
+
+    obj = Time(
+        dados["time"]["nome"],
+        dados["time"]["anoF"],
+        dados["time"]["titulos"],
+        tecnico,
+        contato
+    )
 
     for dadosJogador in dados["jogadores"]:
         jogador = Jogador(
@@ -44,24 +75,36 @@ def CarregarTime():
             dadosJogador["idade"],
             dadosJogador["posicao"],
             dadosJogador["numero"],
-            dadosJogador["contrato"])
+            dadosJogador["contrato"]
+        )
 
         obj.AdicionarJogador(jogador)
+
     return obj
 
 def SalvarBase(obj):
     dados = {
-            "time": {
+        "base": {
             "nome": obj.GetNome(),
             "anoF": obj.GetAnoF(),
             "titulos": obj.GetTitulos(),
-            "tecnico": obj.GetTecnico(),
-            "categoria": obj.GetCategoria()
+            "tecnico": {
+                "nome": obj.GetTecnico().GetNome(),
+                "titulos": obj.GetTecnico().GetTitulos(),
+                "anosDeTrabalho": obj.GetTecnico().GetAnosDeTrabalho(),
+                "idade": obj.GetTecnico().GetIdade(),
+                "time": obj.GetTecnico().GetTime(),
+                "esquema": obj.GetTecnico().GetEsquema()
             },
-    
-            "jogadores": []
-        }
-    
+            "contato" : {
+                        "tipo": obj.GetContato().GetTipo(),
+                        "contato": obj.GetContato().GetContato()
+            },
+            "categoria": obj.GetCategoria()
+        },
+        "jogadores": []
+    }
+
     for jogador in obj.GetJogadores():
         dados["jogadores"].append({
             "nome": jogador.GetNome(),
@@ -70,28 +113,50 @@ def SalvarBase(obj):
             "numero": jogador.GetNumero(),
             "contrato": jogador.GetContrato()
         })
-    
+
     with open("Base.json", "w", encoding="utf-8") as arquivo:
         json.dump(dados, arquivo, indent=4, ensure_ascii=False)
 
 def CarregarBase():
-    with open("Base.json", "r", encoding="utf-8") as arquivo:
+    try:
+        with open("Base.json", "r", encoding="utf-8") as arquivo:
             dados = json.load(arquivo)
-    
-    obj = Time(
-        dados["time"]["nome"],
-        dados["time"]["anoF"],
-        dados["time"]["titulos"],
-        dados["time"]["tecnico"],
-        dados["time"]["categoria"])
-    
+    except (FileNotFoundError, json.JSONDecodeError):
+        tecnico = Tecnico("", "", 0, "", "", "")
+        return Base("", 0, "", "", "", "")
+
+    contato = Contato(
+            dados["base"]["contato"]["tipo"],
+            dados["base"]["contato"]["contato"]
+        )
+
+    tecnico = Tecnico(
+        dados["base"]["tecnico"]["nome"],
+        dados["base"]["tecnico"]["titulos"],
+        dados["base"]["tecnico"]["anosDeTrabalho"],
+        dados["base"]["tecnico"]["idade"],
+        dados["base"]["tecnico"]["time"],
+        dados["base"]["tecnico"]["esquema"]
+    )
+
+    obj = Base(
+        dados["base"]["nome"],
+        dados["base"]["anoF"],
+        dados["base"]["titulos"],
+        tecnico,
+        contato,
+        dados["base"]["categoria"]
+    )
+
     for dadosJogador in dados["jogadores"]:
         jogador = Jogador(
-        dadosJogador["nome"],
-        dadosJogador["idade"],
-        dadosJogador["posicao"],
-        dadosJogador["numero"],
-        dadosJogador["contrato"])
-    
+            dadosJogador["nome"],
+            dadosJogador["idade"],
+            dadosJogador["posicao"],
+            dadosJogador["numero"],
+            dadosJogador["contrato"]
+        )
+
         obj.AdicionarJogador(jogador)
+
     return obj
