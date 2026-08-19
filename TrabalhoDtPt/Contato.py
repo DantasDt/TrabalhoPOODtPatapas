0302ec9011881e1b4bb9cc10466d0cc9e9d3a532
Luan Thomaz Fossati <luangatissimo09@gmail.com>
2026-08-19 20:26:51 -0300

    Enhance Contato class with dialogue feature
    Added dialogue functionality for the Contato class, including a method to generate random dialogues between a coach and a player.

--- TrabalhoDtPt/Contato.py
+++ TrabalhoDtPt/Contato.py
@@ -1,7 +1,12 @@
+import random
+from Time import Time
+
 class Contato:
-    def __init__(self, tipo, contato):
+    def __init__(self, tipo, contato,tecnico, jogador):
         self.__tipo = tipo
         self.__contato = contato
+        self.tecnico = tecnico
+        self.jogador = jogador
 
     def Ligar(self):
         return f"Contato do time: {self.__contato}"
@@ -11,3 +16,25 @@ class Contato:
 
     def GetContato(self):
         return self.__contato
+
+
+    def Dialogo(self):
+        dialogos =[
+                (f"{self.tecnico}: Você está pronto para a partida?", f"{self.jogador}: Sim, professor. Estou preparado!"), 
+                (f"{self.tecnico}: Como você está se sentindo hoje?", f"{self.jogador}: Estou bem e motivado para jogar."), 
+                (f"{self.tecnico}: Preciso que você tenha mais confiança.", f"{self.jogador}: Pode deixar, vou dar o meu melhor."), 
+                (f"{self.tecnico}: Você treinou muito bem esta semana.", f"{self.jogador}: Obrigado, professor. Estou me esforçando."), 
+                (f"{self.tecnico}: Qual posição você prefere jogar?", f"{self.jogador}: Prefiro jogar como atacante."),
+                (f"{self.tecnico}: Hoje precisamos vencer.", f"{self.jogador}: Vamos lutar pela vitória!"), 
+                (f"{self.tecnico}: Não se preocupe com os erros.", f"{self.jogador}: Vou continuar tentando."),
+                (f"{self.tecnico}: Você está evoluindo bastante.", f"{self.jogador}: Fico feliz em ouvir isso."), 
+                (f"{self.tecnico}: Está preparado para entrar em campo?", f"{self.jogador}: Estou pronto!"),
+                (f"{self.tecnico}: Mostre tudo o que aprendeu nos treinos.", f"{self.jogador}: Pode confiar em mim!")
+                ]
+
+dialogo= random.choice(dialogos) 
+print(dialogo[0])
+print(dialogo[1])
+dialogo1 = Contato( "Treinador", "Carlos", "Carlos", "Lucas" )
+
+dialogo1.Dialogo()
